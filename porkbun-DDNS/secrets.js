a281b8cb6ba122f28f6941cde4f2652aa9308fc4
Leigh McKenna <lei@leimckenna.com>
2021-07-06 12:39:12 -0400

    Finish node code for ddns

--- porkbun-DDNS/secrets.js
+++ porkbun-DDNS/secrets.js
@@ -1,2 +1,2 @@
-export const apiKey = pbAPIKey;
-export const secKey = pbSecKey;
+export const DNSAPIKey = pbAPIKey;
+export const DNSSecKey = pbSecKey;
--- porkbun-DDNS/server.js
+++ porkbun-DDNS/server.js
@@ -1,4 +1,4 @@
-import { apiKey, secretKey } from './secrets';
+import { DNSAPIKey, DNSSecKey } from './secrets';
 
 const publicIp = require('public-ip');
 const express = require('express');
@@ -10,23 +10,63 @@ const axios = require('axios');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const timeoutValue = 600;
+const subdomain = "picicd"
+const domain = "cael.tech"
 
 // App
 const app = express();
 
 var currentIp = "";
 
+//utilizes node cron to schedule a task every minute
 cron.schedule('* * * * *', function() {
     let newIp = await publicIp.v4();
     if (newIp != currentIp) {
         currentIp = newIp;
 
-        axios.post('https://porkbun.com/api/json/v3/dns/retrieve/cael.tech', {
-            secretapikey: secretKey,
-            apikey: apiKey
+        //get current records
+        axios.post('https://porkbun.com/api/json/v3/dns/retrieve/' + domain, {
+            secretapikey: DNSSecKey,
+            apikey: DNSAPIKey,
         }).then(res => {
-            console.log(`statusCode: ${res.statusCode}`)
-            console.log(res)
+            let recordArr = res.records;
+            let recordId = "";
+            //see if intended subdomain's record already exists
+            for (let i = 0; i < recordArr.count(); i++){
+                if (subdomain == recordArr[i].name) {
+                    recordId = recordArr[i].id;
+                }
+            }
+            if (recordId != "") {
+                //update record
+                axios.post('https://porkbun.com/api/json/v3/dns/edit/' + domain + '/' + recordId, {
+                    secretapikey: DNSSecKey,
+                    apikey: DNSAPIKey,
+	                name: subdomain,
+	                type: "A",
+	                content: currentIp,
+	                ttl: "300"
+                }).then(res => {
+                    console.log("Record updated at: " + new Date().getTime());
+                }).catch(error => {
+                    console.error(error)
+                })
+            }
+            else {
+                axios.post('https://porkbun.com/api/json/v3/dns/create/' + domain, {
+                    secretapikey: DNSSecKey,
+                    apikey: DNSAPIKey,
+	                name: subdomain,
+	                type: "A",
+	                content: currentIp,
+	                ttl: "300"
+                }).then(res => {
+                    console.log("Record updated at: " + new Date().getTime());
+                }).catch(error => {
+                    console.error(error)
+                })
+            }
+
         }).catch(error => {
             console.error(error)
         })
