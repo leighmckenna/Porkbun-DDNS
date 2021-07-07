794d1e912ca1b049432a2830c6e1e70758e1373b
Leigh McKenna <lei@leimckenna.com>
2021-07-06 22:17:34 -0400

    Finish changes to container for DDNS

--- porkbun-DDNS/server.js
+++ porkbun-DDNS/server.js
@@ -1,10 +1,8 @@
-import { DNSAPIKey, DNSSecKey } from './secrets';
-
-const publicIp = require('public-ip');
-const express = require('express');
-const cron = require('node-cron');
-const https = require('https');
-const axios = require('axios');
+import { DNSAPIKey, DNSSecKey } from './secrets.js';
+import * as publicIp from 'public-ip';
+import express from 'express';
+import cron from 'node-cron';
+import axios from 'axios';
 
 // Constants
 const PORT = 8080;
@@ -19,20 +17,20 @@ const app = express();
 var currentIp = "";
 
 //utilizes node cron to schedule a task every minute
-cron.schedule('* * * * *', function() {
-    let newIp = await publicIp.v4();
-    if (newIp != currentIp) {
-        currentIp = newIp;
+cron.schedule('* * * * *', async function() {
+    var futurIp = await publicIp.v4();
+    if (futurIp != currentIp) {
+        currentIp = futurIp;
 
         //get current records
         axios.post('https://porkbun.com/api/json/v3/dns/retrieve/' + domain, {
             secretapikey: DNSSecKey,
             apikey: DNSAPIKey,
         }).then(res => {
-            let recordArr = res.records;
+            let recordArr = res.data.records;
             let recordId = "";
             //see if intended subdomain's record already exists
-            for (let i = 0; i < recordArr.count(); i++){
+            for (let i = 0; i < recordArr.length; i++){
                 if (subdomain == recordArr[i].name) {
                     recordId = recordArr[i].id;
                 }
