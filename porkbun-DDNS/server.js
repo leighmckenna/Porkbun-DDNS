3bb6c02d37debcd33eb3132d7d17667169365530
Leigh McKenna <lei@leimckenna.com>
2021-07-15 12:09:32 -0500

    Fixes to dns api issue

--- porkbun-DDNS/server.js
+++ porkbun-DDNS/server.js
@@ -18,6 +18,27 @@ var currentIp = "";
 
 //utilizes node cron to schedule a task every minute
 cron.schedule('* * * * *', async function() {
+
+    //if currentIp isn't set yet (ie on start) figure out if it has a record and update it
+    if (currentIp == "") {
+        axios.post('https://porkbun.com/api/json/v3/dns/retrieve/' + domain, {
+            secretapikey: DNSSecKey,
+            apikey: DNSAPIKey,
+        }).then(res => {
+            let recordArr = res.data.records;
+            let recordId = "";
+            //see if intended subdomain's record already exists
+            for (let i = 0; i < recordArr.length; i++){
+                if (subdomain + "." + domain == recordArr[i].name) {
+                    currentIp = recordArr[i].content;
+                }
+            }
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+
+    console.log("Current ip is: " + currentIp);
     var futurIp = await publicIp.v4();
     if (futurIp != currentIp) {
         currentIp = futurIp;
@@ -31,11 +52,11 @@ cron.schedule('* * * * *', async function() {
             let recordId = "";
             //see if intended subdomain's record already exists
             for (let i = 0; i < recordArr.length; i++){
-                if (subdomain == recordArr[i].name) {
+                if (subdomain + "." + domain == recordArr[i].name) {
                     recordId = recordArr[i].id;
                 }
             }
-            console.log(recordId);
+
             if (recordId != "") {
                 //update record
                 axios.post('https://porkbun.com/api/json/v3/dns/edit/' + domain + '/' + recordId, {
@@ -60,7 +81,7 @@ cron.schedule('* * * * *', async function() {
 	                content: currentIp,
 	                ttl: "300"
                 }).then(res => {
-                    console.log("Record updated at: " + new Date().getTime());
+                    console.log("Record created at: " + new Date().getTime());
                 }).catch(error => {
                     console.error(error)
                 });
@@ -69,6 +90,9 @@ cron.schedule('* * * * *', async function() {
         }).catch(error => {
             console.error(error)
         })
+    }
+    else {
+        console.log("IP has not changed. Skipping...")
     }
 });
 
