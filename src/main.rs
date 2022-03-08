287e53a92a2bf0973955e91dd8a5fb4d1f68b91c
Leigh McKenna <leigh@leighmckenna.com>
2022-03-08 01:23:30 -0600

    Final setup for ddns

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,15 @@ struct AuthResponse {
 
 const PUBKEY: &str = "";
 const PRIVKEY: &str = "";
-
+const DOMAIN: &str = "";
+const TTL: &str = "600";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Find current IP
 
-    let res = reqwest::Client::new()
+    let current_ip_res = reqwest::Client::new()
         .post("https://porkbun.com/api/json/v3/ping")
         .body("{\"secretapikey\": \"".to_owned() + &PRIVKEY + "\",\"apikey\": \"" + &PUBKEY + "\"}")
         .send()
@@ -26,19 +27,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .text()
         .await?;
     
-    let auth: AuthResponse = serde_json::from_str(&res)?;
+    let auth: AuthResponse = serde_json::from_str(&current_ip_res)?;
     // println!("body = {:?}", res);
-    println!("{}", auth.yourIp);
+    //println!("{}", auth.yourIp);
 
-    // Check if the IP has changed
+    let set_dns_res = reqwest::Client::new()
+        .post("https://porkbun.com/api/json/v3/dns/editByNameType/".to_owned() + &DOMAIN +"/A")
+        .body("{\"secretapikey\": \"".to_owned() + &PRIVKEY + "\",\"apikey\": \"" + &PUBKEY + "\",\"content\": \"" + &auth.yourIp + "\",\"ttl\": \"" + &TTL + "\"}")
+        .send()
+        .await?
+        .text()
+        .await?;
 
-
-    // If it has, redirect the DNS to the new IP
-    // If it hasn't, do nothing
-
-
+    
+    // println!("{}", set_dns_res);
     Ok(())
 }
-
-
-
